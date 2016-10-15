a337e544a5fccd891c84b774240fedaeb4f4cc50
Abdul Malik Ikhsan <samsonasik@gmail.com>
2016-10-16 05:16:28 +0700

    spacing

--- spec/ToImplement.spec.php
+++ spec/ToImplement.spec.php
@@ -5,16 +5,23 @@ namespace Kahlan\Extra\Matcher\Spec;
 use stdClass;
 
 describe('ToImplement', function () {
+
     describe('::match()', function () {
+
         it('passes if $actual is implements of $expected', function () {
+
             $actual = new Bar();
             $expeted = 'Kahlan\Extra\Matcher\Spec\Foo';
 
             expect($actual)->toImplement($expeted);
+
         });
 
         it('fails passes if $actual is not implement of $expected', function () {
+
             expect(new stdClass())->not->toImplement('Fux');
+
         });
+
     });
 });
