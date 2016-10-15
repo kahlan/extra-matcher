09b2881c4fedf1a334e0b4e25d7f15c312fa7343
Abdul Malik Ikhsan <samsonasik@gmail.com>
2016-10-16 05:04:08 +0700

    cs

--- spec/ToImplement.spec.php
+++ spec/ToImplement.spec.php
@@ -1,31 +1,20 @@
 <?php
+
 namespace Kahlan\Extra\Matcher\Spec;
 
-use Kahlan\Matcher\ToBeAnInstanceOf;
 use stdClass;
 
 describe('ToImplement', function () {
-
     describe('::match()', function () {
-
         it('passes if $actual is implements of $expected', function () {
-            
-            interface foo { }
-            class bar implements foo {}
-                
-            $actual = new bar();
-            $expeted = 'foo';
-            
-            expect($actual)->toImplement('foo');
+            $actual = new Bar();
+            $expeted = 'Foo';
 
+            expect($actual)->toImplement($expeted);
         });
-        
+
         it('fails passes if $actual is not implement of $expected', function () {
-
-            expect(new stdClass())->not->toImplement('fux');
-
+            expect(new stdClass())->not->toImplement('Fux');
         });
-
     });
-
 });
