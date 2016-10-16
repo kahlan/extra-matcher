a776e656b7d42a0554a154fe639be809661c840f
Abdul Malik Ikhsan <samsonasik@gmail.com>
2016-10-16 20:11:48 +0700

    added ExtraMatchers spec

---
+++ spec/ExtraMatchers.spec.php
@@ -0,0 +1,42 @@
+<?php
+namespace Kahlan\Extra\Matcher\Spec;
+
+use Exception;
+use Kahlan\Extra\Matcher\ExtraMatchers;
+use Kahlan\Matcher;
+
+describe('ExtraMatchers', function () {
+
+    describe('::register()', function () {
+
+        it('passes with register mapped matchers', function () {
+
+            Matcher::unregister('toBeOneOf');
+            expect(Matcher::exists('toBeOneOf'))->toBe(false);
+            ExtraMatchers::register(['toBeOneOf']);
+            expect(Matcher::exists('toBeOneOf'))->toBe(true);
+
+        });
+
+        it('passes with register all matchers', function () {
+
+            ExtraMatchers::register();
+            expect(Matcher::exists('toBeOneOf'))->toBe(true);
+            expect(Matcher::exists('toEqualOneOf'))->toBe(true);
+            expect(Matcher::exists('toImplement'))->toBe(true);
+
+        });
+
+        it('fails if matcher is not exists', function () {
+
+            expect(function () {
+                ExtraMatchers::register(['foo']);
+            })->toThrow(new Exception("Unexisting matcher `foo` can't register it."));
+
+            expect(Matcher::exists('foo'))->toBe(false);
+
+        });
+
+    });
+
+});
